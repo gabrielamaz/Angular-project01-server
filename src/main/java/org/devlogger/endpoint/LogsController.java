d401472e4c84459928c2ae1d4c74c6d76f7e47a4
Gabriela Azevedo <gabriela.azevedo12@gmail.com>
2020-01-09 16:41:56 -0300

    Adding post method on LogsController

--- src/main/java/org/devlogger/endpoint/LogsController.java
+++ src/main/java/org/devlogger/endpoint/LogsController.java
@@ -1,11 +1,17 @@
 package org.devlogger.endpoint;
 
+import org.devlogger.model.Logs;
 import org.devlogger.repository.LogsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("logs")
@@ -18,7 +24,13 @@ public class LogsController {
     this.logsDao = logsDao;
   }
 
+  @GetMapping
   public ResponseEntity<?> listAllLogs() {
     return new ResponseEntity<>(logsDao.findAll(), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<?> saveLog(@Valid @RequestBody Logs log) {
+    return new ResponseEntity<>(logsDao.save(log), HttpStatus.OK);
   }
 }
