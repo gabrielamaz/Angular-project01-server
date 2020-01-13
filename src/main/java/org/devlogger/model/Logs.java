0b63ad9619380369cd22f215253a7c3808373273
Gabriela Azevedo <gabriela.azevedo12@gmail.com>
2020-01-13 12:52:56 -0300

    Added GeneratedValue annotation on Logs ID

--- src/main/java/org/devlogger/model/Logs.java
+++ src/main/java/org/devlogger/model/Logs.java
@@ -1,22 +1,24 @@
 package org.devlogger.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 
 @Entity @Data @NoArgsConstructor
 public class Logs {
-  @Id
+  @Id @GeneratedValue
   private long id;
 
   @NotEmpty
   private String text;
 
-  @DateTimeFormat @NotEmpty
+  @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone="GMT")
   private Date date;
 }
